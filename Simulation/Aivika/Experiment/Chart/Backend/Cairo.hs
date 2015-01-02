0c0d6d44c651be56f6aedb2445339f9cc3e6945a
David Sorokin <david.sorokin@gmail.com>
2015-01-02 10:10:42 +0300

    Updated the year in the copyright notice.

--- Simulation/Aivika/Experiment/Chart/Backend/Cairo.hs
+++ Simulation/Aivika/Experiment/Chart/Backend/Cairo.hs
@@ -3,7 +3,7 @@
 
 -- |
 -- Module     : Simulation.Aivika.Experiment.Chart.Backend.Cairo
--- Copyright  : Copyright (c) 2012-2014, David Sorokin <david.sorokin@gmail.com>
+-- Copyright  : Copyright (c) 2012-2015, David Sorokin <david.sorokin@gmail.com>
 -- License    : BSD3
 -- Maintainer : David Sorokin <david.sorokin@gmail.com>
 -- Stability  : experimental
